58a71c811e1f7913b07d4af22897c598885f60b8
Ajay Shankar <ajays@iiitd.ac.in>
2015-04-07 20:04:15 +0530

    Create weighted_histogram.m

---
+++ weighted_histogram.m
@@ -0,0 +1,28 @@
+function [hst] = weighted_histogram( grad_mags, grad_angles, NBins, HLimit )
+
+	% for a cell.
+	% computes N-bin histogram, specifically for HOGging purposes :)
+	
+	% [grad_mag, grad_angle] = imgradient( img );		-- bad idea!
+	
+	if nargin < 4
+		HLimit = 180;
+		if nargin < 3
+			NBins = 9;
+		end
+	end
+		
+	
+	hst = zeros( NBins, 1 );
+	grad_angles( grad_angles == 0 ) = 1;
+	grad_angles = ceil( grad_angles(:)*(NBins/HLimit) );
+	grad_mags = grad_mags(:);
+	for x = 1:length( grad_angles )
+		hst(grad_angles(x)) = hst(grad_angles(x)) + grad_mags(x);
+	end
+	
+	%size(hst)
+	% if you want to see what the histogram of each cell looks like.
+	% fprintf('%0.2f ', hst'); fprintf('\n');
+	% hst'			 
+end
